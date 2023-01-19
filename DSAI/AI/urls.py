6986b9cff0a3201acdd49d03fec9890f19260743
YYYSSG <yyssgg312@gmail.com>
2023-01-19 16:57:51 +0900

    레오에게
    드립니다

--- DSAI/AI/urls.py
+++ DSAI/AI/urls.py
@@ -5,4 +5,7 @@ from AI import views
 urlpatterns = [
     path('main/', views.main, name='main'),
     path('dataset/', views.getTestData, name='getTestData'),
+    path('dataset/post/', views.setTestData, name='setTestData'),
+    path('dataset/update/', views.updateTestData, name='updateTestData'),
+    path('dataset/delete/', views.deleteTestData, name='deleteTestData')
 ]
--- DSAI/AI/views.py
+++ DSAI/AI/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
 from .serializers import DataSerializer
 from .models import Post
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+import datetime
+import json
 
 # Create your views here.
 
@@ -17,3 +18,53 @@ def getTestData(request):
     datas = Post.objects.all()
     serializers = DataSerializer(datas, many=True)
     return Response(serializers.data);
+
+@api_view(['POST'])
+def setTestData(request):
+    if request.method == 'GET':
+        print('GET_WORK')
+    if request.method =='POST':
+        print('SET_WORK')
+    post = Post()
+    data = str(request.data).replace('\'', '\"')
+    data = json.loads(data)
+    post.post_group = data['post_group']
+    post.post_title = data['post_title']
+    post.post_content = data['post_content']
+    post.created_at = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    post.save()
+    return redirect('getTestData')
+
+@api_view(['POST'])
+def updateTestData(request):
+    if request.method == 'GET':
+        print('GET_WORK')
+    if request.method =='POST':
+        print('SET_WORK')
+    data = str(request.data).replace('\'', '\"')
+    data = json.loads(data)
+    target_id = data['target_id']
+    post = Post.objects.get(field_id = target_id)
+    data = data['changes']
+    for key in data.keys():
+        if key == 'post_group':
+            post.post_group = data['post_group']
+        elif key == 'post_title':
+            post.post_title = data['post_title']
+        elif key == 'post_content':
+            post.post_content = data['post_content']
+    post.save()
+    return redirect('getTestData')
+
+@api_view(['POST'])
+def deleteTestData(request):
+    if request.method == 'GET':
+        print('GET_WORK')
+    if request.method =='POST':
+        print('SET_WORK')
+    data = str(request.data).replace('\'', '\"')
+    data = json.loads(data)
+    target_id = data['target_id']
+    post = Post.objects.get(field_id = target_id)
+    post.delete()
+    return redirect('getTestData')
